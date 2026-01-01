31b865dad55de821ced4e01a801ae880e21029a8
Gornemant <gornemant@gmail.com>
2026-01-01 16:34:04 +0100

    Ch3 L2: Draw Player

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import pygame
 from constants import *
 from logger import log_state
+from circleshape import CircleShape
+from player import Player
 
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock() #pygame object to track time
 	dt = 0
+	player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 	while True:
 		log_state()
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill("black")
+		player.draw(screen)
 		pygame.display.flip()
 		dt = clock.tick() / 1000 #returns time passed since last tick in seconds
 		clock.tick(MAX_FPS) #limits the maximum FPS to the set constant
