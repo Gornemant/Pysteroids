531f170499d90472841308e553cf6c7b8367cc4d
Gornemant <gornemant@gmail.com>
2026-01-03 00:06:29 +0100

    Ch4 L1: Asteroids (fixed previous error of using clock.tick twice per frame)

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 from logger import log_state
 from circleshape import CircleShape
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
 	pygame.init()
@@ -11,8 +13,12 @@ def main():
 	dt = 0
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
 	Player.containers = (updatable, drawable)
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = updatable
 	player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+	asteroid_field = AsteroidField()
 	
 	while True:
 		log_state()
@@ -24,8 +30,7 @@ def main():
 		for i in drawable:
 			i.draw(screen)
 		pygame.display.flip()
-		dt = clock.tick() / 1000 #returns time passed since last tick in seconds
-		clock.tick(MAX_FPS) #limits the maximum FPS to the set constant
+		dt = clock.tick(MAX_FPS) / 1000 #returns time passed since last tick in seconds, MAX_FPS limits the maximum FPS to the set constant
 
 
 if __name__ == "__main__":
